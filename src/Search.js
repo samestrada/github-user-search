21ad45ec3c4438f9cc4834d68bea4867bc52a513
Samantha Estrada <samantha.estrada932@gmail.com>
2021-08-01 13:38:50 -0700

    Add pagination

--- src/Search.js
+++ src/Search.js
@@ -1,4 +1,5 @@
 import React, { Component, useState, useEffect } from 'react';
+import ListGroup from 'react-bootstrap/ListGroup';
 
 const API           = 'https://api.github.com/search/users?q=';
 const PAGE_OP       = '&page=';
@@ -14,12 +15,14 @@ class Search extends Component{
         this.state = {
             queryString:'',
             results:[],
-            submitted: false
+            submitted: false,
+            pageNumbers: []
         };
-        this.handleChange = this.handleChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
-        this.getInfo = this.getInfo.bind(this);
-        this.resetState = this.resetState.bind(this);
+        this.handleChange   = this.handleChange.bind(this);
+        this.handleSubmit   = this.handleSubmit.bind(this);
+        this.getInfo        = this.getInfo.bind(this);
+        this.resetState     = this.resetState.bind(this);
+        this.handleClick     = this.handleClick.bind(this);
     }
 
     //as field changes, update queryString
@@ -28,6 +31,17 @@ class Search extends Component{
     }
     //on submit button press, retrieve queried results
     handleSubmit(e){
+        this.fetchResults();
+    }
+
+    handleClick(event) {
+        console.log("event: ", event.target.id);
+        // this.setState({submitted: false})
+        CURRENT_PAGE = event.target.id;
+        this.fetchResults();
+    }
+
+    fetchResults(){
         fetch(API + this.state.queryString + PAGE_OP + CURRENT_PAGE + PER_PAGE_OP + PER_PAGE)
         .then(res => res.json())
         .then(data => {
@@ -45,23 +59,38 @@ class Search extends Component{
 
         //array holding star count, full name (if exists), and follower count in that order.
         var info = [];
+        console.log("yessir");
 
-        fetch(url)
-        .then(res => res.json())
-        .then(data => {
-            info.push(data.name);
-            info.push(data.followers);
-        })
-        fetch(url+'/starred')
-        .then(res => res.json())
-        .then(data => {
-            info.push(data.length);
-        })
-        return info;
+        // fetch(url)
+        // .then(res => res.json())
+        // .then(data => {
+        //     info.push(data.name);
+        //     info.push(data.followers);
+        // })
+        // fetch(url+'/starred')
+        // .then(res => res.json())
+        // .then(data => {
+        //     info.push(data.length);
+        // })
+        // return info;
+    }
+
+    pagination(){
+        this.state.pageNumbers = [];
+        if(this.state.results.total_count > 0){
+            console.log("here");
+            //total page numbers to display
+            console.log("Total results: ", this.state.results.total_count, "Total per page: ", PER_PAGE);
+            console.log("Divide: ", Math.ceil(this.state.results.total_count / PER_PAGE))
+            for(let i  =  0; i < Math.ceil(this.state.results.total_count / PER_PAGE); i++){
+                this.state.pageNumbers.push(i+1);
+            }
+            {console.log("Total pages: ", this.state.pageNumbers)}
+        }
     }
 
     resetState(){
-        this.setState({queryString:'', results: [], refresh: 1});
+        this.setState({queryString:'', results: [], submitted: false, pageNumbers: []});
     }
 
     //This function fetches data for each user in the results list, and sets it up to be rendered after search
@@ -69,27 +98,33 @@ class Search extends Component{
         console.log("results function: ", this.state.results.items)
         var extraDetails = {};
 
+        //for each user, call getInfo to learn follower/star count etc.
         if (typeof this.state.results.items !== "undefined") {
             this.state.results.items.forEach(element => {
                 extraDetails[element.login] = this.getInfo(element.url);
             });
-
         var showing = 0;
+        this.pagination();
         (this.state.results.total_count < PER_PAGE) ? showing = this.state.results.total_count : showing = PER_PAGE;
             return(
-                <>
-                <p>Showing {showing} out of {this.state.results.total_count} results for the search '{this.state.queryString}'</p>
-                <table className="filtered-users">
+                <> 
+
+                <p>{this.state.results.total_count} results for the search '{this.state.queryString}'</p>
+                {this.state.pageNumbers.map(number => (
+                        <button id={number} onClick={this.handleClick}> {number} </button> 
+                    ))} 
+                <ListGroup>
                     {this.state.results.items.map(item => (
-                        <tr>
+                        <ListGroup.Item>
                             {console.log("pfp: ", this.state.results.items.avatar_url)}
                             <img src={this.state.results.items.avatar_url} alt=''/>
                             {/* meant to display username with extradetails (full name, follower/star count), experiencing difficulty producing this */}
                             {/* {extraDetails[item.login][0]} | Followers: {extraDetails[item.login]} */}
                             <td><a href={GITHUB + item.login}>{item.login}</a> </td>
-                        </tr>
+                            </ListGroup.Item>
                     ))}
-                </table>
+                </ListGroup>
+
                 </>
             )
         }

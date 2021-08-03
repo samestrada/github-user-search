125486a3022085226d9df23f290499213d8944c5
Samantha Estrada <samantha.estrada932@gmail.com>
2021-08-02 23:26:05 -0700

    Add in avatars and implement follower/starred/public repo info

--- src/Search.js
+++ src/Search.js
@@ -5,8 +5,9 @@ const API           = 'https://api.github.com/search/users?q=';
 const PAGE_OP       = '&page=';
 const PER_PAGE_OP   = '&per_page=';
 const PER_PAGE      = '25';
-const GITHUB        = 'https://github.com/';
 var CURRENT_PAGE    = '1';
+const TEST          = 'https://6108a740d73c6400170d39e6.mockapi.io/TestFields';
+
 
 class Search extends Component{
     
@@ -16,7 +17,10 @@ class Search extends Component{
             queryString:'',
             results:[],
             submitted: false,
-            pageNumbers: []
+            finished: false,
+            pageNumbers: [],
+            total_count: 0
+
         };
         this.handleChange   = this.handleChange.bind(this);
         this.handleSubmit   = this.handleSubmit.bind(this);
@@ -27,16 +31,20 @@ class Search extends Component{
 
     //as field changes, update queryString
     handleChange(e) {
-        this.setState({queryString: e.target.value});
+        this.resetState();
+        this.setState({
+            queryString: e.target.value, 
+            submitted: false});
     }
     //on submit button press, retrieve queried results
     handleSubmit(e){
         this.fetchResults();
+        this.setState({pageNumbers: []});
     }
 
     handleClick(event) {
-        console.log("event: ", event.target.id);
-        // this.setState({submitted: false})
+        console.log("click event: ", event.target.id);
+        this.setState({submitted: false})
         CURRENT_PAGE = event.target.id;
         this.fetchResults();
     }
@@ -44,48 +52,79 @@ class Search extends Component{
     fetchResults(){
         fetch(API + this.state.queryString + PAGE_OP + CURRENT_PAGE + PER_PAGE_OP + PER_PAGE)
         .then(res => res.json())
-        .then(data => {
-
+        .then(data => {      
           this.setState({
             results: data,
-          }
-          );
-        });
-        this.setState({submitted: true})
+            total_count: data.total_count
+          });
+          console.log("GET fetch for ", this.state);
+        })
     }
+
 
     // This function handles API calls for each individual github user returned in results list
-    getInfo(url){
+    async getInfo(url, pos){
 
         //array holding star count, full name (if exists), and follower count in that order.
-        var info = [];
-        console.log("yessir");
+        // console.log("tryna get some info for ", url);
+        var fetched = {};
+        var copyResults = [];
 
-        // fetch(url)
+        // console.log("COUNTER ", pos)
+        // console.log("State/Result ", this.state.results.items)
+        copyResults = this.state.results.items;
+        // console.log("copy: ", copyResults);
+        let items = {...copyResults};
+        // console.log("item: ", items);
+        let item = items[pos];
+
+        // fetch(TEST)
         // .then(res => res.json())
-        // .then(data => {
-        //     info.push(data.name);
-        //     info.push(data.followers);
+        // .then(data => {      
+        //     item['followers'] = data[0].followers;
+        //     item['stars'] = data[0].stars;
+        //     item['repos'] = data[0].repos;
+        //     copyResults[pos] = item;
+        //     this.setState({results:copyResults});
+            
         // })
-        // fetch(url+'/starred')
-        // .then(res => res.json())
-        // .then(data => {
-        //     info.push(data.length);
-        // })
-        // return info;
+        //fetch for both gen user url and starred url for number of starred repos -- execute in parallel and wait till complete
+        Promise.all([
+            fetch(url, ).then(res => res.json()),
+            fetch(url + '/starred').then(res => { return res.json();})
+        ]).then(([genUserData, starred]) => {
+            console.log("user: ", item);
+            console.log("data to be inserted: ", genUserData.followers)
+            item['followers'] = genUserData.followers;
+            item['stars'] = starred.length;
+            item['repos'] = genUserData.public_repos;
+            copyResults[pos] = item;
+            console.log("RESULTS UPDATED: ", copyResults)
+            this.setState({results:copyResults});
+        })
+        // console.log("Fetched length: ", fetched.length);
+
+        //workaround for fetched array returning before becoming populated
+        // if(!fetched.length){
+        //     console.log("waiting...");
+        //     setTimeout(function() { //Start the timer
+        //         console.log("Fetched array after wait: ", fetched);
+        //         return fetched; //After 0.5 second, return array
+        //     }.bind(this), 1000)
+        // } else{
+        // console.log("Fetched array: ", fetched);
+        // }
     }
 
+
+    //logic for necessary page buttons to display results
     pagination(){
-        this.state.pageNumbers = [];
         if(this.state.results.total_count > 0){
-            console.log("here");
+            // console.log("figuring out pagination...");
             //total page numbers to display
-            console.log("Total results: ", this.state.results.total_count, "Total per page: ", PER_PAGE);
-            console.log("Divide: ", Math.ceil(this.state.results.total_count / PER_PAGE))
             for(let i  =  0; i < Math.ceil(this.state.results.total_count / PER_PAGE); i++){
                 this.state.pageNumbers.push(i+1);
             }
-            {console.log("Total pages: ", this.state.pageNumbers)}
         }
     }
 
@@ -93,70 +132,116 @@ class Search extends Component{
         this.setState({queryString:'', results: [], submitted: false, pageNumbers: []});
     }
 
+
+    showFinal(){
+        if(this.state.results.items){
+            return(<p>Loading...</p>)
+        }
+        console.log("Results final: ", this.state.results)
+        console.log("Results items: ", this.state)
+
+        // this.state.results.items.map(item=>(
+        //     console.log(item.login)
+        // ))
+        return(
+            <> 
+
+            <p>{this.state.total_count} results for the search '{this.state.queryString}'</p>
+            {this.state.pageNumbers.map(number => (
+                    <button id={number} onClick={this.handleClick}> {number} </button> 
+                ))}
+                <ListGroup>
+                    {this.state.results.map(item => (
+                    <ListGroup.Item align = "left">
+                        <img src={item.avatar_url} width="40px" height="40px" alt=''/>
+                        <a href={item.html_url}>{item.login} </a>
+                        | Followers: {item.followers} | Stars: {item.stars} | Public Repos: {item.repos}
+                    </ListGroup.Item>
+                    ))}
+                </ListGroup>
+            {/* <ListGroup>
+                {this.state.results.forEach(item => (
+                   <h1>{item.login}</h1>
+                ))}
+            </ListGroup> */}
+
+            </>
+        )
+    }
+
     //This function fetches data for each user in the results list, and sets it up to be rendered after search
     returnResults(){
-        console.log("results function: ", this.state.results.items)
-        var extraDetails = {};
+        // var fetchLimiter = 0;
+        var pos = 0;
+        var limit = 0;
+        (this.state.results.total_count < 25) ? limit = this.state.results.total_count : limit = 25;
 
         //for each user, call getInfo to learn follower/star count etc.
         if (typeof this.state.results.items !== "undefined") {
+            console.log("triggered false submit: ", this.state.results.items)
+            this.setState({
+                submitted: false
+            })
             this.state.results.items.forEach(element => {
-                extraDetails[element.login] = this.getInfo(element.url);
+                //ensures we only go through fetch cycle once
+                // if (fetchLimiter < 3) {
+                //     console.log("limiter: ", fetchLimiter);
+                // if(typeof this.getInfo(element.url) !== "undefined"){
+                //     console.log("setting now!");
+                    // console.log("Working on element ", element.login);
+
+                    this.getInfo(element.url, pos);
+                    // console.log("Did work? ", this.state.results.items);
+
+                    pos++;
+
             });
-        var showing = 0;
-        this.pagination();
-        (this.state.results.total_count < PER_PAGE) ? showing = this.state.results.total_count : showing = PER_PAGE;
-            return(
-                <> 
 
-                <p>{this.state.results.total_count} results for the search '{this.state.queryString}'</p>
-                {this.state.pageNumbers.map(number => (
-                        <button id={number} onClick={this.handleClick}> {number} </button> 
-                    ))} 
-                <ListGroup>
-                    {this.state.results.items.map(item => (
-                        <ListGroup.Item>
-                            {console.log("pfp: ", this.state.results.items.avatar_url)}
-                            <img src={this.state.results.items.avatar_url} alt=''/>
-                            {/* meant to display username with extradetails (full name, follower/star count), experiencing difficulty producing this */}
-                            {/* {extraDetails[item.login][0]} | Followers: {extraDetails[item.login]} */}
-                            <td><a href={GITHUB + item.login}>{item.login}</a> </td>
-                            </ListGroup.Item>
-                    ))}
-                </ListGroup>
+        console.log("HEY??????????????????????????", this.state.results.items)
+        if(pos === limit){
+            this.setState({
+                submitted: true
+            })
+        }
 
-                </>
-            )
+        //prevent from overriding previous pagination
+        if(this.state.pageNumbers.length < 1){
+            this.pagination();
+        }
+
+        return(<p>Search through all Github users!</p>)
+            
         }
         
     }
 
     render(){
-
+        console.log("Reloading... ");
+        console.log("Reload state: ", this.state.results);
         //render search result count only if we have actual results
-        if(typeof this.state.results.items !== "undefined" && this.state.refresh === 1){
-            console.log("Total matches found: ", this.state.results.total_count);
+        // if(typeof this.state.results.items !== "undefined" && this.state.refresh === 1){
+        //     console.log("DO WE EVER GET HERE ");
             
-            this.resetState();
-        }
+        //     this.resetState();
+        // }
 
         return(
             <div>
-                <form className="searchBar" onSubmit={this.handleSubmit}>
+                <form className="searchBar">
                     <input
                         type='text'
-                        name='q='
+                        name='q'
                         className='input'
                         placeholder='Search for users'
-                        value={this.state.queryString}
+                        // value={this.state.queryString}
                         onKeyPress={(e) => e.key === 'Enter' && e.preventDefault()} //require submission through submit button
                         onChange={this.handleChange}
                         required
                      />
                  <input type="button" value="Search" onClick={this.handleSubmit}/>
                 </form>
-                {this.state.submitted ? this.returnResults()
-                    : <p>Search through all Github users!</p>}
+                {this.state.submitted ? this.showFinal()
+                    : this.returnResults()}
              </div>   
         );
     }
